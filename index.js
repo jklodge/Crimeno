ea786761ab1a0f9ff018481fb4c37e3ce8950e7d
Jess Lodge <jessicaklodge@gmail.com>
2018-04-05 09:25:41 +0100

    Fixing port for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.use(express.static(`${__dirname}/public`));
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
