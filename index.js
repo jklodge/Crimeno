4070f905e5610c62c2313c34eae0f8779d89ef37
Jess Lodge <jessicaklodge@gmail.com>
2018-05-13 12:26:16 +0100

    added CNAME

--- index.js
+++ index.js
@@ -18,5 +18,4 @@ app.use('/api', router);
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
-
 module.exports = app;
